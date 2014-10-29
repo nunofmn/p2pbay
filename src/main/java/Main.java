52fc34b9696748d9c751213671b6f641b1444002
Nuno Nogueira <nunofmn@gmail.com>
2014-10-29 16:39:31 +0000

    Added simple P2PBay example.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import core.PeerConnection;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws NumberFormatException, Exception {
+
+        PeerConnection peercore = new PeerConnection(Integer.parseInt(args[0]));
+
+        if(args.length == 3) {
+            peercore.store(args[1], args[2]);
+        }
+
+        if(args.length == 2) {
+           System.out.println("Name:" + args[1] + " IP:" + peercore.get(args[1]));
+        }
 
         System.out.println("P2PBay - Coming Soon");
 
---
+++ src/main/java/core/PeerConnection.java
@@ -0,0 +1,46 @@
+package core;
+
+import net.tomp2p.futures.FutureBootstrap;
+import net.tomp2p.futures.FutureDHT;
+import net.tomp2p.p2p.Peer;
+import net.tomp2p.p2p.PeerMaker;
+import net.tomp2p.peers.Number160;
+import net.tomp2p.storage.Data;
+
+import java.io.IOException;
+
+public class PeerConnection {
+
+    final private Peer peer;
+
+    public PeerConnection(int peerId)
+            throws Exception {
+
+        peer = new PeerMaker(Number160.createHash(peerId)).setPorts(4000+peerId).makeAndListen();
+        FutureBootstrap fb = peer.bootstrap().setBroadcast().setPorts(4001).start();
+        fb.awaitUninterruptibly();
+
+        if(fb.getBootstrapTo() != null) {
+            peer.discover().setPeerAddress(fb.getBootstrapTo().iterator().next()).start().awaitUninterruptibly();
+        }
+    }
+
+    public String get(String name)
+        throws ClassNotFoundException, IOException {
+
+        FutureDHT futureDHT = peer.get(Number160.createHash(name)).start();
+        futureDHT.awaitUninterruptibly();
+        if(futureDHT.isSuccess()) {
+            return futureDHT.getData().getObject().toString();
+        }
+
+        return "not found";
+    }
+
+    public void store(String name, String ip)
+        throws IOException {
+
+        peer.put(Number160.createHash(name)).setData(new Data(ip)).start().awaitUninterruptibly();
+    }
+
+}
