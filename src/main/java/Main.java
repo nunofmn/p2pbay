a7a9e0066716ac5d7d56d609d1636d52a9c7b882
joaoSampaio <joaoSampaio>
2014-11-01 23:30:33 +0000

    Login funcional em pcs na mesma rede

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,73 @@
+import core.model.NetworkContent;
+import core.model.UserProfile;
 import core.network.PeerConnection;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args)
             throws NumberFormatException, Exception {
 
-        PeerConnection peercore = new PeerConnection(Integer.parseInt(args[0]));
+        String user;
+        String pass;
+        NetworkContent userProfile;
+        PeerConnection peercore = new PeerConnection(args[0], args[1], args[2]);
 
-        if(args.length == 3) {
-            peercore.store(args[1], args[2]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Bem vindo a' sua loja P2P  - Leilões");
+        while(true) {
+            System.out.println("Pressione a tecla pretendida:");
+            System.out.println("1 - Login");
+            System.out.println("2 - Criar utilizador");
+            System.out.println("3 - Sair");
+            String escolha = br.readLine();
+            if (escolha.equals("1")) {
+                System.out.println("Por favor digite o seu username");
+                user = br.readLine();
+                System.out.println("digite a sua password");
+                pass = br.readLine();
+
+                userProfile = peercore.get(user);
+                if (userProfile != null && userProfile.contentType().equals("User")) {
+                    if (((UserProfile) userProfile).login(pass)) {
+                        System.out.println("Login  - Sucesso!");
+                        System.out.println("Login  - Mostrar mais menu agora!");
+                    } else {
+                        System.out.println("Login  - A password fornecida não é igual!");
+                    }
+                } else {
+                    System.out.println("utilizador não encontrado!");
+                    continue;
+                }
+            } else if (escolha.equals("2")) {
+                System.out.println("Formulario de registo novo utilizador:");
+                while (true) {
+                    System.out.println("Digite o seu username pretendido:");
+                    user = br.readLine();
+                    userProfile = peercore.get(user);
+                    if (userProfile != null) {
+                        System.out.println("Nome de utilizador ja em uso, escolha um diferente:");
+                        continue;
+                    }
+
+                    System.out.println("Digite a sua password:");
+                    pass = br.readLine();
+                    if(user.isEmpty() || pass.isEmpty()){
+                        System.out.println("Username e password não podem estar vazios!");
+                        continue;
+                    }
+
+                    peercore.store(user, new UserProfile(pass));
+                    break;
+                }
+            } else if (escolha.equals("3")){
+                System.exit(0);
+            }
+
+            System.out.println("P2PBay - Coming Soon");
         }
-
-        if(args.length == 2) {
-           System.out.println("Name:" + args[1] + " IP:" + peercore.get(args[1]));
-        }
-
-        System.out.println("P2PBay - Coming Soon");
-
     }
 
 }
