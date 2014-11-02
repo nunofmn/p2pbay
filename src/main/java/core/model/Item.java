674876ca07518eb0dde9e7bf87bb2be5854a8893
1cgmr <cgmr.321@gmail.com>
2014-11-02 18:06:02 +0000

    item

--- src/main/java/core/model/Item.java
+++ src/main/java/core/model/Item.java
@@ -1,4 +1,45 @@
 package core.model;
+import java.util.ArrayList;
+import  java.util.List;
 
 public class Item extends NetworkContent {
+
+    private double value;
+    private int id;
+    private List<BidHistoryInfo> Bids;
+
+
+    public Item(String description, int id, int value) {
+        super();
+        this.description = description;
+        this.value = value;
+        this.id = id;
+        Bids = new ArrayList<BidHistoryInfo>();
+    }
+
+    public void addBid(BidHistoryInfo bid){
+        Bids.add(bid);
+    }
+
+    public void printBidHistoryInfo(){
+        for(BidHistoryInfo bid : Bids) {
+            System.out.println();
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
 }
+
